fa35e76e234d710ddb01b2899945f332599182a5
adamkoy <adam.koyuncu@vechain.org>
2025-03-07 08:08:25 -0300

    new account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "sandbox-2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "adam.koyuncu+sandbox-2@live.com"
+    AccountName               = "sandbox-adam-dev-2"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "adam.koyuncu+sandbox-2@live.com"
+    SSOUserFirstName          = "Sandbox-2"
+    SSOUserLastName           = "Adam-Dev-2"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox-2"
+}
